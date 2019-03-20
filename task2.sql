44d9e5720167635a5716993c42303090730f8037
berta <berta.vincze@gmail.com>
2019-03-20 10:51:22 +0100

    Add Task 2 solution

---
+++ task2.sql
@@ -0,0 +1,1 @@
+SELECT company_name AS Company, COUNT(product_name) as Products from products JOIN suppliers on products.supplier_id = suppliers.supplier_id GROUP BY suppliers.company_name ORDER BY Products DESC, Company ASC;
