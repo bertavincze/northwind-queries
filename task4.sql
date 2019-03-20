2591b999d917ffa82e30734e441b1df25a5f18fc
berta <berta.vincze@gmail.com>
2019-03-20 14:04:22 +0100

    Add Task 4 solution

---
+++ task4.sql
@@ -0,0 +1,4 @@
+SELECT company_name AS company, ARRAY_AGG(order_id) AS order_id
+FROM customers JOIN orders ON customers.customer_id = orders.customer_id
+GROUP BY customers.company_name
+ORDER BY company ASC;
