07491bea64608e25f7024b1a1251016ca5c97cf1
Berta <44132652+bertavincze@users.noreply.github.com>
2019-04-04 11:37:21 +0200

    Replace JOIN with LEFT JOIN

--- task4.sql
+++ task4.sql
@@ -1,4 +1,4 @@
 SELECT company_name AS company, ARRAY_AGG(order_id) AS order_id
-FROM customers JOIN orders ON customers.customer_id = orders.customer_id
+FROM customers LEFT JOIN orders ON customers.customer_id = orders.customer_id
 GROUP BY customers.company_name
 ORDER BY company ASC;
